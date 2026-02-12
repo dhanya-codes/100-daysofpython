22138b99bedadc766b6ef8c732ecdc5323d5f573
dhanya-codes <2401107021@ptuniv.edu.in>
2026-02-11 21:37:49 -0800

    String operation program added

---
+++ string.py/string.ex.2.py
@@ -0,0 +1,7 @@
+word="Dhanya is a Very Good Girl"
+vowel=['a','e','i','o','u','A','E','I','O','u']
+count=0
+for i in word:
+    if i in vowel:
+        count+=1
+print("The number of vowels is",count)        
